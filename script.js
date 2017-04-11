51350ae5069bbf3f80992ecf91782535d80acb5b
anibaka9 <anibaka9@gmail.com>
2017-04-11 14:59:47 +0300

    Fix element checking and add left items

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 $(document).ready(function() {
-  
+  var num_last = 0;
   $('#editer').keydown(function(e) {
   	if (e.keyCode == 13) {
         e.preventDefault();
         var list_el = $('<li><div class="list-element el-not-edit"><form class="pure-form todo-edit-form"><fieldset><input type="checkbox" class="rec-check"><label class="todo-item">' + $(this).val() + '</label><input class="todo-edit" type="text">')
         $('#Todo-List').append(list_el);
         $(this).val('')
-        $('.rec-check').click(function () {
-    $(this).parent().parent().parent().toggleClass('checked');
-  })
-  $('.todo-item').dblclick(function () {
-    $(this).parent().parent().parent().addClass('editing');
-    $(this).parent().parent().parent().removeClass('el-not-edit');
-    $(this).siblings('.todo-edit').val($(this).text())
-  })
-  $('.todo-edit').keydown(function(e) {
-    if (e.keyCode == 13) {
-        e.preventDefault();
-        $(this).siblings('.todo-item').text($(this).val())
-        $(this).parent().parent().parent().removeClass('editing');
-        $(this).parent().parent().parent().addClass('el-not-edit');
-    }
-  });
+        num_last += 1;
+        $('#app-fotter').text(num_last + ' items left');
     };
-  });
+});
+
+$(document).on('click', '.rec-check', function () {
+  if (this.checked) {
+    $(this).parent().parent().parent().addClass('checked');
+    num_last -= 1;
+    $('#app-fotter').text(num_last + ' items left');
+  }
+  else {
+    $(this).parent().parent().parent().removeClass('checked');
+    num_last += 1;
+    $('#app-fotter').text(num_last + ' items left');
+  };
+});
+$(document).on('dblclick', '.todo-item', function () {
+   $(this).parent().parent().parent().addClass('editing');
+   $(this).parent().parent().parent().removeClass('el-not-edit');
+   $(this).siblings('.todo-edit').val($(this).text())
+});
+$(document).on('keydown', '.todo-edit', function(e) {
+  if (e.keyCode == 13) {
+    e.preventDefault();
+    $(this).siblings('.todo-item').text($(this).val())
+    $(this).parent().parent().parent().removeClass('editing');
+    $(this).parent().parent().parent().addClass('el-not-edit');
+  };
+});
 })
